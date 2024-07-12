87d72ae27b695c5a4a75e436c6317a7e2d7c8b0c
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-12 11:35:26 +0300

    İş katmanı kuruldu.

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -11,8 +11,31 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityrepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfProductDal : IProductDal
     {
-       
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> GetList(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
